b59fc8caf7240cfbdd0289088479645c3a818561
jkdearnley <jkdearnley@gmail.com>
2020-12-10 17:49:07 +1300

    filters

--- app/app.js
+++ app/app.js
@@ -8,10 +8,42 @@ myCheeseApp.run(function(){
 
 });
 
-myCheeseApp.controller('CheeseController', function($scope){
+myCheeseApp.controller('CheeseController', ['$scope', function($scope){
 
     $scope.message= "Hey y'all";
 
-    $scope.cheeses = ['Brie', 'Camembert', 'Parmesan', 'Cheddar', 'Mozzarella'];
+    // $scope.cheeses = ['Brie', 'Camembert', 'Parmesan', 'Cheddar', 'Mozzarella'];
+    $scope.cheeses = [
+        {
+            name: "Brie",
+            firmness: "soft",
+            taste: "medium",
+            price: 7
+        },
+        {
+            name: "Camembert",
+            firmness: "soft",
+            taste: "medium",
+            price: 6.50
+        },
+        {
+            name: "Cheddar",
+            firmness: "firm",
+            taste: "strong",
+            price: 4
+        },
+        {
+            name: "Parmesan",
+            firmness: "hard",
+            taste: "sharp",
+            price: 10
+        },
+        {
+            name: "Mozzarella",
+            firmness: "soft",
+            taste: "mild",
+            price: 9
+        }
+    ]
 
-});
+}]);
